815a9a0b5c2296367db8220e9881b488661014d7
kamleshray55 <88548609+kamleshray55@users.noreply.github.com>
2021-11-21 02:06:12 +0530

    Create jenkins.groovy

---
+++ vars/jenkins.groovy
@@ -0,0 +1,15 @@
+def call(stringe stageName){
+
+    if ( "${stageName}"=="build")
+    {
+        sh 'mvn clean package'
+    }
+    else if ( "${stageName}"== "SonarQube Report")
+    {
+        sh 'mvn sonar:sonar'
+    }
+    else if ( "${stageName}" == "Upload Into Nexus")
+    {
+        sh 'mvn deploy'
+    }
+}
